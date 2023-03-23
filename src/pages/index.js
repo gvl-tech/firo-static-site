b55265024e8cd865f93b983c305957f9e22b75aa
codeyluke <lukekennedy212@gmail.com>
2023-03-23 23:11:41 +0800

    temp remove

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,6 @@ import { Layout } from "../infrastructure/layouts/Layout"
 import { theme } from "../infrastructure/theme"
 import "../styles/global.css"
 import Home1 from "./sections/Home1"
-import Home2 from "./sections/Home2"
-import Home3 from "./sections/Home3"
-import Home4 from "./sections/Home4"
 
 function index() {
   return (
@@ -14,9 +11,6 @@ function index() {
       <ThemeProvider theme={theme}>
         <Layout inverse={true}>
           <Home1 />
-          <Home2 />
-          <Home3 />
-          <Home4 />
         </Layout>
       </ThemeProvider>
     </>
