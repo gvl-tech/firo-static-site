6294c3599f8c1ca342fb87494f6bebdbb38b1f11
codeyluke <lukekennedy212@gmail.com>
2023-03-24 00:38:44 +0800

    Revert "test"
    This reverts commit 272ba8516b0bc158e9d1cf6ba46bbde6d6c1eea6.

--- src/components/utilities/padded-div.component.js
+++ src/components/utilities/padded-div.component.js
@@ -1,11 +1,11 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 const PaddedContainer = (theme, x, y, bg, br) => `  
-  padding: ${theme.space[y]}px ${theme.space[x]}px;
+  padding: ${theme.space[y ? y : 3]}px ${theme.space[x ? x : 3]}px;
   background-color: ${bg};
   border-radius: ${theme.borderSizes[br]}px;
-`
+`;
 
 export const PaddedDiv = styled.div`
   ${({ theme, x, y, bg, br }) => PaddedContainer(theme, x, y, bg, br)}
-`
+`;
