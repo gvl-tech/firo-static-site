76b1b3d74870e6ca41e0c904644141bc26e5bfd0
darrenteoh <darren.teoh@govirallab.com>
2023-04-12 15:03:25 +0800

    Added header and footer to latest updates and FAQ page

--- src/templates/blog-index-template.js
+++ src/templates/blog-index-template.js
@@ -1,7 +1,9 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
+import { Layout } from "../infrastructure/layouts/Layout"
 import BlogCard from "../pages/blog/components/BlogCard"
 import "../styles/blogIndex.less"
+
 const BlogIndexTemplate = ({ pageContext }) => {
   const { pageCount, group, index } = pageContext
   const previousIndex = index - 1
@@ -12,77 +14,79 @@ const BlogIndexTemplate = ({ pageContext }) => {
   const nextPageUrl = `/latest-updates/${nextIndex}`
 
   return (
-    <div id="blogIndex">
-      <section class="page-intro section">
-        <div class="row">
-          <div class="col">
-            <span>Latest </span>
-            <span class="red">Updates</span>
-          </div>
-        </div>
-      </section>
-
-      <section class="blog-nav">
-        <div class="container">
-          <ul class="row">
-            <li>
-              <Link
-                to="/latest-updates"
-                activeStyle={{ color: "white", backgroundColor: "#9b1c2e" }}
-              >
-                All
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/tag/news"
-                activeStyle={{ color: "white", backgroundColor: "#9b1c2e" }}
-              >
-                News
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/tag/dev"
-                activeStyle={{ color: "white", backgroundColor: "#9b1c2e" }}
-              >
-                Dev
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/tag/community"
-                activeStyle={{ color: "white", backgroundColor: "#9b1c2e" }}
-              >
-                Community
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      {group?.map(post => (
-        <BlogCard key={post.id} post={post} />
-      ))}
-
-      <section class="blog-section">
-        <div class="container">
-          <div class="pagination">
-            <div class="text-center page-numbers">
-              <p>
-                <Link to={previousPageUrl} class="page-button">
-                  &laquo; Prev
-                </Link>
-                {index} of {pageCount}
-                <Link to={nextPageUrl} class="page-button">
-                  Next &raquo;
-                </Link>
-              </p>
+    <Layout>
+      <div id="blogIndex">
+        <section class="page-intro section">
+          <div class="row">
+            <div class="col">
+              <span>Latest </span>
+              <span class="red">Updates</span>
             </div>
           </div>
-        </div>
-      </section>
-    </div>
+        </section>
+
+        <section class="blog-nav">
+          <div class="container">
+            <ul class="row">
+              <li>
+                <Link
+                  to="/latest-updates"
+                  activeStyle={{ color: "white", backgroundColor: "#9b1c2e" }}
+                >
+                  All
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/tag/news"
+                  activeStyle={{ color: "white", backgroundColor: "#9b1c2e" }}
+                >
+                  News
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/tag/dev"
+                  activeStyle={{ color: "white", backgroundColor: "#9b1c2e" }}
+                >
+                  Dev
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/tag/community"
+                  activeStyle={{ color: "white", backgroundColor: "#9b1c2e" }}
+                >
+                  Community
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </section>
+
+        {group?.map(post => (
+          <BlogCard key={post.id} post={post} />
+        ))}
+
+        <section class="blog-section">
+          <div class="container">
+            <div class="pagination">
+              <div class="text-center page-numbers">
+                <p>
+                  <Link to={previousPageUrl} class="page-button">
+                    &laquo; Prev
+                  </Link>
+                  {index} of {pageCount}
+                  <Link to={nextPageUrl} class="page-button">
+                    Next &raquo;
+                  </Link>
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </Layout>
   )
 }
 
--- src/templates/tags-index-template.js
+++ src/templates/tags-index-template.js
@@ -2,73 +2,76 @@ import React from "react"
 import { graphql, Link } from "gatsby"
 import BlogCard from "../pages/blog/components/BlogCard"
 import "../styles/blogIndex.less"
+import { Layout } from "../infrastructure/layouts/Layout"
 
 const TagsIndexTemplate = ({ pageContext, data }) => {
   const { tag } = pageContext
   const { edges } = data.allMarkdownRemark
 
   return (
-    <div id="blogIndex">
-      <section class="page-intro section">
-        <div class="row">
-          <div class="col">
-            <span>Latest </span>
-            <span class="red">Updates</span>
+    <Layout>
+      <div id="blogIndex">
+        <section class="page-intro section">
+          <div class="row">
+            <div class="col">
+              <span>Latest </span>
+              <span class="red">Updates</span>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-      <section class="blog-nav">
-        <div class="container">
-          <ul class="row">
-            <li>
-              <Link
-                to="/latest-updates"
-                activeStyle={{ color: "white", backgroundColor: "#9b1c2e" }}
-              >
-                All
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/tag/news"
-                activeStyle={{ color: "white", backgroundColor: "#9b1c2e" }}
-              >
-                News
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/tag/dev"
-                activeStyle={{ color: "white", backgroundColor: "#9b1c2e" }}
-              >
-                Dev
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/tag/community"
-                activeStyle={{ color: "white", backgroundColor: "#9b1c2e" }}
-              >
-                Community
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <section class="page-tag section">
-        <div class="row">
-          <div class="col">
-            <h1>#{tag}</h1>
+        <section class="blog-nav">
+          <div class="container">
+            <ul class="row">
+              <li>
+                <Link
+                  to="/latest-updates"
+                  activeStyle={{ color: "white", backgroundColor: "#9b1c2e" }}
+                >
+                  All
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/tag/news"
+                  activeStyle={{ color: "white", backgroundColor: "#9b1c2e" }}
+                >
+                  News
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/tag/dev"
+                  activeStyle={{ color: "white", backgroundColor: "#9b1c2e" }}
+                >
+                  Dev
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/tag/community"
+                  activeStyle={{ color: "white", backgroundColor: "#9b1c2e" }}
+                >
+                  Community
+                </Link>
+              </li>
+            </ul>
           </div>
-        </div>
-      </section>
+        </section>
 
-      {edges.map(post => (
-        <BlogCard key={post.node.id} post={post.node} />
-      ))}
-    </div>
+        <section class="page-tag">
+          <div class="row">
+            <div class="col">
+              <h1>#{tag}</h1>
+            </div>
+          </div>
+        </section>
+
+        {edges.map(post => (
+          <BlogCard key={post.node.id} post={post.node} />
+        ))}
+      </div>
+    </Layout>
   )
 }
 
